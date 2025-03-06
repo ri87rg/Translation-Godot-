77679a5dadfe80f8813e8153b4688d4ac1759644
ri87rg <134312763+ri87rg@users.noreply.github.com>
2025-03-06 13:01:21 +0300

    Add files via upload

--- translation/Main-Menu/Scripts/languagesManager.gd
+++ translation/Main-Menu/Scripts/languagesManager.gd
@@ -1,11 +1,8 @@
 extends Control
 
-func _on_english_pressed() -> void:
-	TranslationServer.set_locale("en")
-
-func _on_arabic_pressed() -> void:
-	TranslationServer.set_locale("ar")
-
-
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main-Menu/Settings/options.tscn")
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Quiz-Homescreen/Quiz.tscn")
